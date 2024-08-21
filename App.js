52946cd6bc624f0573dbe4bc9aedcb46c15359b9
Augusto <augustoortolan02@gmail.com>
2024-08-21 19:48:52 -0300

    Revisão do useState com app contador

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
 
 export default function App() {
+  const [contador, setContador] = useState(0);
+
+  function somar() {
+    const novoValor = contador + 1;
+    setContador(novoValor);
+  }
+
+  function subtrair() {
+    const novoValor = contador - 1;
+    setContador(novoValor);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity style={styles.button} onPress={() => subtrair()}>
+        <Text style={styles.buttonText}>Diminuir</Text>
+      </TouchableOpacity>
+      <Text style={{ fontSize: 50 }}>{contador}</Text>
+      <TouchableOpacity style={styles.button} onPress={() => somar()}>
+        <Text style={styles.buttonText}>Somar</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +32,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  button: {
+    width: 100,
+    height: 80,
+    backgroundColor: "green",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 10,
+  },
+  buttonText: {
+    fontSize: 25,
+    color: "white",
+    fontWeight: "bold",
   },
 });
